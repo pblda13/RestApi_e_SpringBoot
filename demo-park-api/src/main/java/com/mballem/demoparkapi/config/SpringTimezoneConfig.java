3897e9a2eebfd17d86cb9354407729645e099dcd
Pâmela Borges <pamela_borges18@hotmail.com>
2023-11-01 13:38:25 -0300

    Configurações iniciais do projeto

---
+++ demo-park-api/src/main/java/com/mballem/demoparkapi/config/SpringTimezoneConfig.java
@@ -0,0 +1,18 @@
+package com.mballem.demoparkapi.config;
+
+import java.util.TimeZone;
+
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.annotation.PostConstruct;
+
+@Configuration
+public class SpringTimezoneConfig {
+	
+	
+	@PostConstruct
+	public void timezoneConfig() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+	}
+
+}
