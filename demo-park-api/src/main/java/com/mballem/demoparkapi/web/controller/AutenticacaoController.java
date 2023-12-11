d2877920df17f5dcd5fc78018c32550645a9ee81
Pâmela Borges <pamela_borges18@hotmail.com>
2023-12-11 11:55:42 -0300

    Documentar e testar o sistema de autenticação

--- demo-park-api/src/main/java/com/mballem/demoparkapi/web/controller/AutenticacaoController.java
+++ demo-park-api/src/main/java/com/mballem/demoparkapi/web/controller/AutenticacaoController.java
@@ -1,10 +1,15 @@
 package com.mballem.demoparkapi.web.controller;
 
-import com.mballem.demoparkapi.entity.Usuario;
 import com.mballem.demoparkapi.jwt.JwtToken;
 import com.mballem.demoparkapi.jwt.JwtUserDetailsService;
 import com.mballem.demoparkapi.web.dto.UsuarioLoginDto;
+import com.mballem.demoparkapi.web.dto.UsuarioResponseDto;
 import com.mballem.demoparkapi.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Autenticação", description = "Recurso para proceder com a autenticação na API")
 @Slf4j
 @RequiredArgsConstructor
 @RestController
@@ -27,23 +33,33 @@ public class AutenticacaoController {
 
     private final JwtUserDetailsService detailsService;
     private final AuthenticationManager authenticationManager;
-@PostMapping("/auth")
-    public ResponseEntity<?> autenticar(@RequestBody @Valid UsuarioLoginDto dto, HttpServletRequest request){
-        log.info("Processo de autenticação pelo login{}",dto.getUsername());
 
+    @Operation(summary = "Autenticar na API", description = "Recurso de autenticação na API",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Autenticação realizada com sucesso e retorno de um bearer token",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UsuarioResponseDto.class))),
+                    @ApiResponse(responseCode = "400", description = "Credenciais inválidas",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Campo(s) Inválido(s)",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
+    @PostMapping("/auth")
+    public ResponseEntity<?> autenticar(@RequestBody @Valid UsuarioLoginDto dto, HttpServletRequest request) {
+        log.info("Processo de autenticação pelo login {}", dto.getUsername());
         try {
             UsernamePasswordAuthenticationToken authenticationToken =
-                    new UsernamePasswordAuthenticationToken(dto.getUsername(),dto.getPassword());
+                    new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword());
+
             authenticationManager.authenticate(authenticationToken);
+
             JwtToken token = detailsService.getTokenAuthenticated(dto.getUsername());
 
             return ResponseEntity.ok(token);
-
-        }catch (AuthenticationException ex){
-            log.warn("Bad Credentials from username'{}'",dto.getUsername());
+        } catch (AuthenticationException ex) {
+            log.warn("Bad Credentials from username '{}'", dto.getUsername());
         }
         return ResponseEntity
                 .badRequest()
-                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST,"Credenciais Invalidas"));
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, "Credenciais Inválidas"));
     }
 }
